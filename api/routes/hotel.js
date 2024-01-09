4d58de6df93ef44f0084bef998d43b3387f09048
Highkage-30 <kaushikap3001@gmail.com>
2024-01-10 02:42:21 +0530

    dynamic hotel display

--- api/routes/hotel.js
+++ api/routes/hotel.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { createHotel, deleteHotel, getAllHotel, getHotel, updateHotel } from "../controllers/hotelController.js";
+import { countByCity, countbyType, createHotel, deleteHotel, getAllHotel, getHotel, updateHotel } from "../controllers/hotelController.js";
 import { verifyAdmin } from "../utils/verify.js";
 
 //This function is used when you want to create a new router object in your program to handle requests. 
@@ -8,12 +8,15 @@ const router=express.Router();
 //Create using async function as creating schema may take time
 router.post("/",verifyAdmin,createHotel) 
 // Update data , pass the id through url params
-router.put("/:id",verifyAdmin,updateHotel)
+router.put("/find/:id",verifyAdmin,updateHotel)
 //Delete hotel
-router.delete("/:id",verifyAdmin,deleteHotel)
+router.delete("/find/:id",verifyAdmin,deleteHotel)
 //get one
-router.get("/:id",getHotel)
+router.get("/find/:id",getHotel)
 // get all id
 router.get("/",getAllHotel)
+router.get("/countByCity",countByCity)
+router.get("/countByType",countbyType)
+
 
 export default router;
