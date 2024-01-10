77a1ce1ff502898659923f34e3b2bfd25a9368e5
Highkage-30 <kaushikap3001@gmail.com>
2024-01-10 23:24:08 +0530

    final commit

--- api/index.js
+++ api/index.js
@@ -1,62 +1,51 @@
-import  express from "express"; 
+import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 import authRoute from "./routes/auth.js";
 import usersRoute from "./routes/users.js";
-import hotelRoute from "./routes/hotel.js";
+import hotelsRoute from "./routes/hotels.js";
 import roomsRoute from "./routes/rooms.js";
 import cookieParser from "cookie-parser";
-//craeting a variable for express framework
-const app=express()
+import cors from "cors";
 
-//to access .env file
-dotenv.config()
+const app = express();
+dotenv.config();
 
-//connect variable to connect to mongodb using mongoose.connect function and .env file
-//async keyword allows the use of await keyword
-//await will pause execution till we get value in return from mongoose.connect
-const connect=async()=>{
-    try {
-        await mongoose.connect(process.env.MONGO)
-        console.log("Connected to Mongodb")
-    } catch (error) {
-        throw error
-    }
-}
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO);
+    console.log("Connected to mongoDB.");
+  } catch (error) {
+    throw error;
+  }
+};
 
-// mongoose.connection.on("Disconnected",()=>{
-//     console.log("Mongodb Disconnected")
-// })
+mongoose.connection.on("disconnected", () => {
+  console.log("mongoDB disconnected!");
+});
 
-// mongoose.connection.on("Connected",()=>{
-//     console.log("Mongodb Connected")
-// })
-
-
-
-//Middleware-Middleware gets executed after the server receives the request and before the controller actions send the response.
+//middlewares
+app.use(cors())
 app.use(cookieParser())
-app.use(express.json())
+app.use(express.json());
 
-app.use("/api/auth",authRoute)
-app.use("/api/users",usersRoute)
-app.use("/api/rooms",roomsRoute)
-app.use("/api/hotel",hotelRoute)
-//error handling middleware
-app.use((err,req,res,next)=>{
-    const errorMessage=err.message || "Something went wrong"
-    const errorStatus=err.status || 500
-    return res.status(errorStatus).json({
-        success:false,
-        status:errorStatus,
-        message:errorMessage,
-        stack:err.stack  
+app.use("/api/auth", authRoute);
+app.use("/api/users", usersRoute);
+app.use("/api/hotels", hotelsRoute);
+app.use("/api/rooms", roomsRoute);
 
-    })
-})
-//listen to the connections on the specified host and port.
-app.listen(8000,()=>{
-    connect()
-    console.log("Connected to backend")
-})
+app.use((err, req, res, next) => {
+  const errorStatus = err.status || 500;
+  const errorMessage = err.message || "Something went wrong!";
+  return res.status(errorStatus).json({
+    success: false,
+    status: errorStatus,
+    message: errorMessage,
+    stack: err.stack,
+  });
+});
 
+app.listen(8800, () => {
+  connect();
+  console.log("Connected to backend.");
+});
--- api/routes/rooms.js
+++ api/routes/rooms.js
@@ -1,7 +1,28 @@
 import express from "express";
+import {
+  createRoom,
+  deleteRoom,
+  getRoom,
+  getRooms,
+  updateRoom,
+  updateRoomAvailability,
+} from "../controllers/room.js";
+import { verifyAdmin } from "../utils/verifyToken.js";
 
+const router = express.Router();
+//CREATE
+router.post("/:hotelid", verifyAdmin, createRoom);
 
-//This function is used when you want to create a new router object in your program to handle requests. 
-const router=express.Router();
+//UPDATE
+router.put("/availability/:id", updateRoomAvailability);
+router.put("/:id", verifyAdmin, updateRoom);
+//DELETE
+router.delete("/:id/:hotelid", verifyAdmin, deleteRoom);
+//GET
+
+router.get("/:id", getRoom);
+//GET ALL
+
+router.get("/", getRooms);
 
 export default router;
